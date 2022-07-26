6ed8672781272c037abcebee8fe1b2a3ce231c6d
Elf <Elf@Elf.local>
2022-07-26 22:23:45 +0400

    4

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -255,6 +255,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(/* arr */) {
+  throw new Error('Not implemented');
   // const res = [];
   // arr.reduce((prev, cur, i) => res[i] = prev + cur, 0);
   // return res;
@@ -476,6 +477,7 @@ function sortCitiesArray(arr) {
  *           [0,0,0,0,1]]
  */
 function getIdentityMatrix(/* n */) {
+  throw new Error('Not implemented');
   // function matrix(n) {
   //   const res = [];
   //   let i;
